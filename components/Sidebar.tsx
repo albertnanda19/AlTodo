17851ab3397e5ae6920630769f18ece89fda8a29
albert19_ <albertnanda1902@gmail.com>
2023-12-21 11:08:22 +0800

    create global style and global context provider

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,11 +1,26 @@
 "use client"
-
 import React from 'react'
+import styled from 'styled-components'
+import { useGlobalState } from '@/context/globalProvider'
 
 const Sidebar = () => {
+
+    const { theme } = useGlobalState();
+
+    console.log(theme);
+
     return (
-        <div>Sidebar</div>
+        <SidebarStyled theme={theme}>Sidebar</SidebarStyled>
     )
 }
+
+const SidebarStyled = styled.nav`
+    position: relative;
+    width: ${(props) => props.theme.sidebarWidth};
+    background-color: ${(props) => props.theme.colorBg2};
+    border: 2px solid ${(props) => props.theme.borderColor2};
+
+    border-radius: 1rem;
+`;
 
 export default Sidebar
--- providers/ContextProvider.tsx
+++ providers/ContextProvider.tsx
@@ -1,17 +1,31 @@
 "use client";
 
-import React from 'react'
-import GlobalStyleProvider from './GlobalStyleProvider';
+import React, { useEffect, useState } from 'react'
+import { GlobalProvider } from '@/context/globalProvider';
 
 interface Props {
     children: React.ReactNode;
 }
 
 const ContextProvider = ({ children }: Props) => {
+
+    const [isReady, setIsReady] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setIsReady(true)
+        }, 200);
+    }, []);
+
+    if (!isReady) {
+        return null;
+    }
+
+
     return (
-        <GlobalStyleProvider>
+        <GlobalProvider>
             {children}
-        </GlobalStyleProvider>
+        </GlobalProvider>
     )
 }
 
