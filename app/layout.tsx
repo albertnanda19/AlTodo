099e31dbe9a72e882396a8850a081b78d0e395eb
albert19_ <albertnanda1902@gmail.com>
2023-12-21 17:39:54 +0800

    create middleware for clerk provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import Sidebar from '@/components/Sidebar'
 import GlobalStyleProvider from '@/providers/GlobalStyleProvider'
 import ContextProvider from '@/providers/ContextProvider'
+import { ClerkProvider } from '@clerk/nextjs'
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,20 +16,22 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <head>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
-      </head>
-      <body>
-        <ContextProvider>
-          <GlobalStyleProvider>
-            <Sidebar />
-            <div className='w-full'>
-              {children}
-            </div>
-          </GlobalStyleProvider>
-        </ContextProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <head>
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+        </head>
+        <body>
+          <ContextProvider>
+            <GlobalStyleProvider>
+              <Sidebar />
+              <div className='w-full'>
+                {children}
+              </div>
+            </GlobalStyleProvider>
+          </ContextProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
